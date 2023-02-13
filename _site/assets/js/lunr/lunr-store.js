0c72ea516be98b9cc36afdf7c73e30fb502cf62e
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-13 20:16:44 +0900

    test for ruby server

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "안녕하세요, 첫 posting이자, test posting 입니다.",
+        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)     ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      }]
