5c9d8721017f1f714aceff91d2446298011e543c
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-17 23:09:12 +0900

    insert sns link

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "안녕하세요, 첫 posting이자, test posting 입니다.",
-        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)     ","categories": [],
+        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)   hello kkobugi word!     ","categories": [],
         "tags": [],
         "url": "/first/",
         "teaser": null
