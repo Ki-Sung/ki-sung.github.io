cc3e02b590b79c94664f56da79a15f56480c6f2a
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-18 17:03:37 +0900

    add category tags

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "안녕하세요, 첫 posting이자, test posting 입니다.",
-        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)   hello kkobugi word!     ","categories": [],
-        "tags": [],
-        "url": "/first/",
+        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)   hello kkobugi word!     ","categories": ["coding"],
+        "tags": ["Python","blog","jekyll"],
+        "url": "/coding/first/",
         "teaser": null
       }]
