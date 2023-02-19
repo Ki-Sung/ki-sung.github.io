82fcd93470f056f07045defbb53ee1ee8d0eb681
Gilbert/Ki-Sung <kcs4912@gmail.com>
2023-02-19 09:02:26 +0900

    add sidebar, search function

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,1 @@
-var store = [{
-        "title": "안녕하세요, 첫 posting이자, test posting 입니다.",
-        "excerpt":"오늘 처음 블로그를 만들었습니다.   앞으로 열심히 기록해보겠습니다. :)   hello kkobugi word!  이미지 목차   이미지 세부 목차 1  이미지 입니다.   이미지 세부 목차 2  이미지 입니다.   이미지 세부 목차 3  이미지 입니다.     ","categories": ["coding"],
-        "tags": ["Python","blog","jekyll"],
-        "url": "/coding/first/",
-        "teaser": null
-      }]
+var store = []
